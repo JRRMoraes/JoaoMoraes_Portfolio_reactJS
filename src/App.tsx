44a4511a0b217f55ad5da2f081c00f71e92aa127
Dani e Jão <daniejao10@gmail.com>
2023-10-02 01:27:16 -0300

    2023 - ajustes 3 - obtendo Uteis

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from "react"
-import Apresentacao from "./modulos/apresentacao"
-import Experiencia from "./modulos/curriculo_experiencia"
-import ConsumoJsonBruto from "./modulos/consumoJsonBruto"
-import ConsumoJson from "./modulos/consumoJson"
+// import Apresentacao from "./modulos/apresentacao"
+// import Experiencia from "./modulos/curriculo_experiencia"
+// import ConsumoJsonBruto from "./modulos/consumoJsonBruto"
+// import ConsumoJson from "./modulos/consumoJson"
+
+import estilo from "./App.module.scss"
 
 
-export function App(props) {
+export function App() {
 		return (
-				<div>
-						<Apresentacao />
+				<div className={estilo.app}>
+					ola enfermeira
+						{/* <Apresentacao />
 						<Experiencia />
 						<ConsumoJsonBruto />
-						<ConsumoJson />
+						<ConsumoJson /> */}
 				</div>
 		)
 }
